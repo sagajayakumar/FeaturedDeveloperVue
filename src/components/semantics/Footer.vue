1dffe2dd30f70dde58af52575ffb51a982bfd850
Jaya Kumar <jayakumar1994@gmail.com>
2021-11-05 08:31:45 -0500

    changes to footer

--- src/components/semantics/Footer.vue
+++ src/components/semantics/Footer.vue
@@ -2,24 +2,23 @@
   <div class="footer">
   <div><a href="https://github.com/shivaramkothapally/featured-developers-proposal"> 
   <font-awesome-icon :icon="['fab', 'github']" class="icon alt"/> </a>
+
+  <a href="https://github.com/sagajayakumar/FeaturedDeveloperVue"> 
+  Front End </a>
+
+  <a href="https://github.com/sagajayakumar/FeaturedDeveloper3A/tree/master"> 
+  Back End </a>
   
-  Featured Developers</div>
+  3A Featured Developers</div>
     </div>
+  
 </template>
 <script>
-import { defineComponent } from '@vue/composition-api'
+import { defineComponent } from "@vue/composition-api";
 
 export default defineComponent({
- 
-
-  
-   plugins: [
-        
-        "~/plugins/font-awesome",
-        
-    ]
-  
-})
+  plugins: ["~/plugins/font-awesome"],
+});
 </script>
 
 
