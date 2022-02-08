24d1d2051eec4c6a46e67e601c265d3b39fcd372
shivaram kothapally <shivaramkothapally177@gmail.com>
2022-02-08 14:53:07 -0600

    create new component for create developer

--- src/components/CreateDeveloper.vue
+++ src/components/CreateDeveloper.vue
@@ -1,67 +1,137 @@
 <template>
-    <div>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="card">
-                        <div class="card-header">
-                            <h4 class="card-title">Create Developer</h4>
-                        </div>
-                        <div class="card-body">
-                            <form action=createDeveloper method="POST">
-                                <div class="form-group">
-                                    <label for="name">Developer ID</label>
-                                    <input type="text" class="form-control" id="id" name="id" placeholder="Developer ID">
-                                </div>
-                                <div class="form-group">
-                                    <label for="name">Name</label>
-                                    <input type="text" class="form-control" id="name" name="name" placeholder="Name">
-                                </div>
-                                <div class="form-group">
-                                    <label for="email">Email</label>
-                                    <input type="email" class="form-control" id="email" name="email" placeholder="Email">
-                                </div>
-                                <div class="form-group">
-                                    <label for="goals">Goals</label>
-                                    <input type="goals" class="form-control" id="goals" name="goals" placeholder="goals">
-                                </div>
-                                <div class="form-group">
-                                    <label for="Skills">Skills</label>
-                                    <input type="Skills" class="form-control" id="Skills" name="Skills" placeholder="Skills">
-                                </div>
-                                 <div class="form-group">
-                                    <label for="linkedin">linkedIn URL</label>
-                                    <input type="linkedin" class="form-control" id="linkedin" name="linkedin" placeholder="linkedin URL">
-                                </div>
-                                 <div class="form-group">
-                                    <label for="Git">Git URL</label>
-                                    <input type="Git" class="form-control" id="Git" name="linkedin" placeholder="Git URL">
-                                </div>
-                                  <div class="form-group">
-                                    <label for="Image">Image URL</label>
-                                    <input type="Image" class="form-control" id="Image" name="Image" placeholder="Image URL">
-                                </div>
-                                <button type="submit" class="btn btn-primary">Submit</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
+  <div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="card">
+            <div class="card-header">
+              <h4 class="card-title">Create Developer</h4>
             </div>
+            <div class="card-body">
+              
+                <div class="form-group">
+                  <label for="name">Developer ID</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="id"
+                    name="id"
+                    placeholder="Developer ID"
+                    v-model="developerid"
+                  />
+                </div>
+                <div class="form-group">
+                  <label for="name">Name</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="name"
+                    name="name"
+                    placeholder="Name"
+                    v-model="fullname"
+                  />
+                </div>
+                <div class="form-group">
+                  <label for="email">Email</label>
+                  <input
+                    type="email"
+                    class="form-control"
+                    id="email"
+                    name="email"
+                    placeholder="Email"
+                    v-model="emailaddress"
+                  />
+                </div>
+                <div class="form-group">
+                  <label for="goals">Goals</label>
+                  <input
+                    type="goals"
+                    class="form-control"
+                    id="goals"
+                    name="goals"
+                    placeholder="goals"
+                    v-model="goals"
+                  />
+                </div>
+                <div class="form-group">
+                  <label for="Skills">Skills</label>
+                  <input
+                    type="Skills"
+                    class="form-control"
+                    id="Skills"
+                    name="Skills"
+                    placeholder="Skills"
+                    v-model="skills"
+                  />
+                </div>
+                <div class="form-group">
+                  <label for="linkedin">linkedIn URL</label>
+                  <input
+                    type="linkedin"
+                    class="form-control"
+                    id="linkedin"
+                    name="linkedin"
+                    placeholder="linkedin URL"
+                  />
+                </div>
+                <div class="form-group">
+                  <label for="Git">Git URL</label>
+                  <input
+                    type="Git"
+                    class="form-control"
+                    id="Git"
+                    name="linkedin"
+                    placeholder="Git URL"
+                    v-model="githuburl"
+                  />
+                </div>
+                <div class="form-group">
+                  <label for="Image">Image URL</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="Image"
+                    name="Image"
+                    placeholder="Image URL"
+                    v-model="developer.image"
+                  />
+                </div>
+                <button type="submit" v-on:click="developers" class="btn btn-primary">Submit</button>
+              
+            </div>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 <script>
-import { defineComponent } from '@vue/composition-api'
+import { defineComponent } from "@vue/composition-api";
 
 export default defineComponent({
-    name: "createDeveloper",
-   methods: {
-    async developers() {
-      await this.$axios
-        .post(`https://localhost:44312/api/developer`)
-        //.get(`https://featureddeveloperapis.herokuapp.com/api/DeveloperofTheDay`)
+  name: "createDeveloper",
+  data() {
+    return {
+      developer: {
+        developerid: "",
+        fullname: "",
+        emailaddress: "",
+        goals: "",
+        skills: "",
+        linkedin: "",
+        githuburl: "",
+        image: "",
+      },
+    };
+  },
+  methods: {
+     developers() {
+         console.log(this.developer + "1212");
+       this.$axios
+        .post(`https://localhost:44312/api/developer`, this.developer)
+        //.get(`https://featureddeveloperapis.herokuapp.com/api/developer`)
         .then((res) => {
-          console.log(res);
+          console.log(this.developer);
           if (res.status == 200) {
             this.data = res;
             console.log(this.data.data.length + " is the length");
@@ -69,6 +139,5 @@ export default defineComponent({
         });
     },
   },
- },
-)
+});
 </script>
